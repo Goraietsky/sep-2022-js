64cac9b2ceac636cc7b455b60429cd4a4d53bc83
Goraietsky <goraietskyyura@gmail.com>
2022-11-01 17:43:31 +0200

    Initial commit

---
+++ Homework 1/Homework1.js
@@ -0,0 +1,54 @@
+let s1 = 'hello';
+console.log(s1);
+
+let s2 = 'owu';
+console.log(s2);
+
+let s3 = 'com';
+console.log(s3);
+
+let s4 = 'ua'
+console.log(s4);
+
+let n1 = 1;
+console.log(n1);
+
+let n2 = 10;
+console.log(n2);
+
+let n3 = -999;
+console.log(n3)
+
+let n4 = 123;
+console.log(n4);
+
+let n5 =3.14;
+console.log(n5)
+
+let n6 =2.7;
+console.log(n6)
+
+let n7 =16;
+console.log(n7)
+
+let t =100==100;
+console.log(t)
+
+let f = 100 > 400;
+console.log(f);
+
+let firstName = 'Horaietsky Yura Stepanovich';
+let middleName = 'Horaietsky Yura Stepanovich';
+let lastName = 'Horaietsky Yura Stepanovich';
+
+let person =''
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c)
